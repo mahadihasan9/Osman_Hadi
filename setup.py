3a675b637d649c0a0c52dc3e02a554c1276fc1b4
Mahadi Hasan <islammdmahadi943@gmail.com>
2026-01-02 20:55:28 +0600

    Fix project name to lowercase for PyPI

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Osman_Hadi",
+    name="osman_hadi",
     version="1.0.0",
-    author="Your Name",
+    author="Mahdi bin iqbal",
     author_email="islammdmahadi943@gmail.com",
     description="A bilingual Python library documenting the life and legacy of Shaheed Osman Bin Hadi.",
     long_description=open("README.md", encoding="utf-8").read(),
